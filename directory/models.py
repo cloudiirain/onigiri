53dabd458b123453ef231990d6a6afff230befc1
cloudiirain <muffinzapplepear@gmail.com>
2015-04-05 00:12:00 -0400

    basic test add

--- directory/models.py
+++ directory/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 class Series(models.Model):
-    pass
+    title = models.CharField(max_length=200, default="", unique=True)
+    author = models.CharField(max_length=50, default="")
+    artist = models.CharField(max_length=50, default="", blank=True)
+
+    def __unicode__(self):
+        return self.title
 
 class Volume(models.Model):
     pass
--- directory/tests.py
+++ directory/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
 
-# Create your tests here.
+from directory.models import Series, Volume, Chapter
+
+class SimpleTest(TestCase):
+    def test_basic_addition(self):
+        # Tests that 1 + 1 always equals 2.
+        self.assertEqual(1 + 1, 2)
+
+class SeriesTest(TestCase):
+
+    def test_create_series(self):
+        # Create Series
+        series = Series()
+
+        # Set the attributes
+        series.title = 'Ore no Imouto ga Konna ni Kawaii Wake ga Nai'
+        series.author = 'Tsukasa Fushimi'
+        series.artist = 'Hiro Kanzaki'
+
+        # Save it
+        series.save()
+
+        # Check if we can find it
+        all_series = Series.objects.all()
+        self.assertEquals(len(all_series), 1)
+        only_series = all_series[0]
+        self.assertEquals(only_series, series)
+
+        # Check attributes
+        self.assertEquals(only_series.title, 'Ore no Imouto ga Konna ni Kawaii Wake ga Nai')
+        self.assertEquals(only_series.author, 'Tsukasa Fushimi')
+        self.assertEquals(only_series.artist, 'Hiro Kanzaki')
