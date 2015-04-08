9d7ddcf8f3f7ee701d98105551f67ec573104d4f
cloudiirain <muffinzapplepear@gmail.com>
2015-04-08 03:02:43 -0400

    Added slug field searching, and pretty much done with basic directory stuff

--- directory/models.py
+++ directory/models.py
@@ -1,22 +1,31 @@
 from django.core.urlresolvers import reverse
+from django.utils.text import slugify
 from django.db import models
 
 class Series(models.Model):
-    title = models.CharField(max_length=200, default="", unique=True)
+    title = models.CharField(max_length=100, default="")
     author = models.CharField(max_length=50, default="")
     artist = models.CharField(max_length=50, default="", blank=True)
+    slug = models.SlugField(max_length=100, default="", unique=True)
 
     def __unicode__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('series-detail', kwargs={'pk': self.pk})
+        return reverse('series-detail-slug', kwargs={'slug': self.slug})
+
+    def series_slug(self):
+        return self.slug
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Series, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ['title']
 
 class Volume(models.Model):
-    title = models.CharField(max_length=200, default="")
+    title = models.CharField(max_length=100, default="")
     number = models.FloatField(null=True)
     series = models.ForeignKey(Series, null=True)
 
@@ -24,7 +33,10 @@ class Volume(models.Model):
         return str(self.series) + ": " + self.title
 
     def get_absolute_url(self):
-        return reverse('series-detail', kwargs={'pk': self.series.pk})
+        return reverse('series-detail-slug', kwargs={'slug': self.series.slug})
+
+    def series_slug(self):
+        return self.series.slug
 
     class Meta:
         unique_together = ('series', 'title')
@@ -36,7 +48,13 @@ class Chapter(models.Model):
     volume = models.ForeignKey(Volume, null=True)
 
     def __unicode__(self):
-        return self.volume + " " + self.title
+        return str(self.volume) + " " + self.title
+
+    def get_absolute_url(self):
+        return reverse('series-detail-slug', kwargs={'slug': self.volume.series.slug})
+
+    def series_slug(self):
+        return self.volume.series.slug
 
     class Meta:
         unique_together = ('volume', 'title')
--- directory/urls.py
+++ directory/urls.py
@@ -3,6 +3,7 @@ __author__ = 'cloudiirain'
 from django.conf.urls import url, patterns
 from directory.views import SeriesListView, SeriesDetailView, SeriesCreate, SeriesUpdate, SeriesDelete
 from directory.views import VolumeCreate, VolumeUpdate, VolumeDelete
+from directory.views import ChapterCreate, ChapterUpdate, ChapterDelete
 
 urlpatterns = patterns('directory',
     url(r'^$', SeriesListView.as_view(), name='series-list'),
@@ -13,7 +14,10 @@ urlpatterns = patterns('directory',
     url(r'^volume/new/$', VolumeCreate.as_view(), name='volume-add'),
     url(r'^volume/(?P<pk>\d+)/edit/$', VolumeUpdate.as_view(), name='volume-update'),
     url(r'^volume/(?P<pk>\d+)/delete/$', VolumeDelete.as_view(), name='volume-delete'),
-    url(r'^chapter/new/$', 'views.editchapter', name='newchapter'),
-    url(r'^chapter/(?P<pk>\d+)/edit/$', 'views.editchapter', name='editchapter'),
-    url(r'^(?P<title>\w+)/$', 'views.title', name='title'),
+    url(r'^chapter/new/$', ChapterCreate.as_view(), name='chapter-add'),
+    url(r'^chapter/(?P<pk>\d+)/edit/$', ChapterUpdate.as_view(), name='chapter-update'),
+    url(r'^chapter/(?P<pk>\d+)/delete/$', ChapterDelete.as_view(), name='chapter-delete'),
+    url(r'^(?P<slug>[\w\-]+)/$', SeriesDetailView.as_view(), name='series-detail-slug'),
+    url(r'^(?P<slug>[\w\-]+)/edit$/', SeriesUpdate.as_view(), name='series-update-slug'),
+    url(r'^(?P<slug>[\w\-]+)/delete$/', SeriesDelete.as_view(), name='series-delete-slug'),
 )
