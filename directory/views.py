6f7024dc0d70019866c35bc2407a84cde56b17b3
cloudiirain <muffinzapplepear@gmail.com>
2015-04-08 03:02:43 -0400

    Added slug field searching, and pretty much done with basic directory stuff

--- directory/views.py
+++ directory/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, FormView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
+
+from django.utils.text import slugify
 
 from directory.models import Series, Volume, Chapter
 from directory.forms import SeriesForm
@@ -33,6 +35,15 @@ class VolumeCreate(CreateView):
     template_name = "directory/form.html"
     fields = ['title', 'number', 'series']
 
+    def get_initial(self):
+        if self.request.method == 'GET' and 's' in self.request.GET:
+            series = get_object_or_404(Series, pk=self.request.GET['s'])
+            return {
+                'series':series,
+            }
+        else:
+            return None
+
 class VolumeUpdate(UpdateView):
     model = Volume
     template_name = "directory/form.html"
@@ -40,12 +51,32 @@ class VolumeUpdate(UpdateView):
 
 class VolumeDelete(DeleteView):
     model = Volume
-    context_object_name = "object"
     template_name = "directory/confirm_delete.html"
     success_url = reverse_lazy('series-list')
 
-def editchapter(request):
-    pass
+class ChapterCreate(CreateView):
+    model = Chapter
+    template_name = "directory/form.html"
+    fields = ['title', 'number', 'volume']
+
+    def get_initial(self):
+        if self.request.method == 'GET' and 'v' in self.request.GET:
+            volume = get_object_or_404(Volume, pk=self.request.GET['v'])
+            return {
+                'volume':volume,
+            }
+        else:
+            return None
+
+class ChapterUpdate(UpdateView):
+    model = Chapter
+    template_name = "directory/form.html"
+    fields = ['title', 'number', 'volume']
+
+class ChapterDelete(DeleteView):
+    model = Chapter
+    template_name = "directory/confirm_delete.html"
+    success_url = reverse_lazy('series-list')
 
 def title(request):
     pass
