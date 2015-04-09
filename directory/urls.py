1273025b36b43b92054815671d89f9c29563da91
cloudiirain <muffinzapplepear@gmail.com>
2015-04-09 11:27:50 -0400

    Added category tags

--- directory/urls.py
+++ directory/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url, patterns
 from directory.views import SeriesListView, SeriesDetailView, SeriesCreate, SeriesUpdate, SeriesDelete
 from directory.views import VolumeCreate, VolumeUpdate, VolumeDelete
 from directory.views import ChapterCreate, ChapterUpdate, ChapterDelete
+from directory.views import TagCreate, TagUpdate, TagDelete
 
 urlpatterns = patterns('directory',
     url(r'^$', SeriesListView.as_view(), name='series-list'),
@@ -12,18 +13,25 @@ urlpatterns = patterns('directory',
     url(r'^series/(?P<pk>\d+)/edit/$', 'views.series_edit', name='series-update'),
     #url(r'^series/(?P<pk>\d+)/edit/$', SeriesUpdate.as_view(), name='series-update'),
     url(r'^series/(?P<pk>\d+)/delete/$', SeriesDelete.as_view(), name='series-delete'),
+    url(r'^series/(?P<slug>[\w\-]+)/$', SeriesDetailView.as_view(), name='series-detail-slug'),
+    url(r'^series/(?P<slug>[\w\-]+)/edit$/', SeriesUpdate.as_view(), name='series-update-slug'),
+    url(r'^series/(?P<slug>[\w\-]+)/delete$/', SeriesDelete.as_view(), name='series-delete-slug'),
+
     url(r'^volume/new/$', VolumeCreate.as_view(), name='volume-add'),
     url(r'^volume/(?P<pk>\d+)/edit/$', VolumeUpdate.as_view(), name='volume-update'),
     url(r'^volume/(?P<pk>\d+)/delete/$', VolumeDelete.as_view(), name='volume-delete'),
+
     url(r'^chapter/new/$', ChapterCreate.as_view(), name='chapter-add'),
     url(r'^chapter/(?P<pk>\d+)/edit/$', ChapterUpdate.as_view(), name='chapter-update'),
     url(r'^chapter/(?P<pk>\d+)/delete/$', ChapterDelete.as_view(), name='chapter-delete'),
 
+    url(r'^tag/new/$', TagCreate.as_view(), name='tag-add'),
+    url(r'^tag/(?P<pk>\d+)/edit/$', TagUpdate.as_view(), name='tag-update'),
+    url(r'^tag/(?P<pk>\d+)/delete/$', TagDelete.as_view(), name='tag-delete'),
+
     url(r'^search/$', 'views.search', name='search'),
 
-    url(r'^(?P<slug>[\w\-]+)/$', SeriesDetailView.as_view(), name='series-detail-slug'),
-    url(r'^(?P<slug>[\w\-]+)/edit$/', SeriesUpdate.as_view(), name='series-update-slug'),
-    url(r'^(?P<slug>[\w\-]+)/delete$/', SeriesDelete.as_view(), name='series-delete-slug'),
+
 
 
 )
