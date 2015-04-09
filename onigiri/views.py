4739590f8cec17848ac9c7fc28b0f668422eb343
cloudiirain <muffinzapplepear@gmail.com>
2015-04-09 19:25:43 -0400

    Homepage styled (again)

--- onigiri/views.py
+++ onigiri/views.py
@@ -1,5 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render
+from directory.forms import SearchForm
 
 def home(request):
-    return render(request, 'onigiri/index.html')
+    searchform = SearchForm()
+    return render(request, 'onigiri/index.html', {'searchform' : searchform})
