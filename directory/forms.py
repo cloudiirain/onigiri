620668ea01873829ed272c5706f9aedf7b25bb26
cloudiirain <muffinzapplepear@gmail.com>
2015-04-10 00:07:11 -0400

    Fixed missing jquery, switched to django-bootstrap3

--- directory/forms.py
+++ directory/forms.py
@@ -11,5 +11,5 @@ class SearchForm(forms.Form):
     query = forms.CharField(label='',
                             max_length=100,
                             widget=forms.TextInput(
-                                attrs={'class' : 'form-control', 'placeholder' : 'Search',}
+                                attrs={'class' : 'form-control', 'placeholder' : 'search for light novel fan translations',}
                             ))
