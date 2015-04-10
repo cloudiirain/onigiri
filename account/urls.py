e73cae660d1a699b6587370edbaadf518d87a70c
cloudiirain <muffinzapplepear@gmail.com>
2015-04-10 03:06:02 -0400

    Added login stuff, favicon

--- account/urls.py
+++ account/urls.py
@@ -4,9 +4,10 @@ from django.conf.urls import url, patterns, include
 from django.contrib.auth import views
 
 urlpatterns = patterns('account',
+    url(r'^logout/$', 'views.logout', name='logout'),
     url('^', include('django.contrib.auth.urls')),
     url(r'^register/$', 'views.register', name='register'),
-    url(r'^account/$', 'views.cpanel', name='cpanel'),
+    url(r'^account/$', 'views.dashboard', name='dashboard'),
 #    url(r'^search/$', 'views.search', name='search'),
 
 )
