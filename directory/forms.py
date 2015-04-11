9c1f9ccd11f169f1f717ca4d04d1a13e00017956
cloudiirain <muffinzapplepear@gmail.com>
2015-04-11 17:41:56 -0400

    New Volume Detail Page

--- directory/forms.py
+++ directory/forms.py
@@ -1,8 +1,9 @@
-__author__ = 'jeffrey'
+__author__ = 'cloudiirain'
 
 from django import forms
+from django.forms import ModelForm
 from django.forms.models import inlineformset_factory
-from directory.models import Series, Volume, AltTitle
+from directory.models import Series, Volume, AltTitle, Tags
 
 SeriesVolumeFormSet = inlineformset_factory(Series, Volume, fields=('title', 'number'), extra=2)
 SeriesTitleFormSet = inlineformset_factory(Series, AltTitle, fields=('title',), extra=3)
@@ -13,3 +14,25 @@ class SearchForm(forms.Form):
                             widget=forms.TextInput(
                                 attrs={'class' : 'form-control', 'placeholder' : 'search for light novel fan translations',}
                             ))
+
+class TagsForm(ModelForm):
+    class Meta:
+        model = Tags
+        fields = ['title']
+
+class SeriesForm(ModelForm):
+    class Meta:
+        model = Series
+        fields = ['title', 'author', 'artist', 'tags', 'image', 'synopsis']
+
+
+class TitleForm(ModelForm):
+    class Meta:
+        model = AltTitle
+        fields = ['title']
+
+class VolumeForm(ModelForm):
+    pass
+
+class ChapterForm(ModelForm):
+    pass
